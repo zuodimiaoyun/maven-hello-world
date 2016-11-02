e692f081d2ec2fe2ee242c3d6be6bdca2b644964
pollos <zuodimiaoyun@gmail.com>
2016-11-02 05:06:41 -0400

    maven init

---
+++ src/main/java/com/anansi/action/App.java
@@ -0,0 +1,13 @@
+package com.anansi.action;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
